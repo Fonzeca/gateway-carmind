a1c57b7b1476e2026a104075d5f402c61d5db2da
Fonzeca <alexisfonzos_@hotmail.com>
2023-11-23 16:02:55 -0300

    Update docker-compose command to force recreate containers and fix API endpoint

--- scripts/auth.lua
+++ scripts/auth.lua
@@ -2,8 +2,8 @@ local http = require "resty.http"
 
 local httpc = http.new()
 
-local res, err = httpc:request_uri("https://tu-servicio-rest.com/ruta", {
-    method = "GET", -- o "POST", "PUT", etc., según lo que necesites
+local res, err = httpc:request_uri("http://user-hub/validate", {
+    method = "POST", -- o "POST", "PUT", etc., según lo que necesites
     headers = {
         ["Content-Type"] = "application/json", -- ajusta los encabezados según tus necesidades
     },
