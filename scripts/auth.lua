01d3af173d99491e09be414ceebb417278423eea
Fonzeca <alexisfonzos_@hotmail.com>
2023-11-24 09:13:13 -0300

    Update user-hub URL in auth.lua

--- scripts/auth.lua
+++ scripts/auth.lua
@@ -2,7 +2,7 @@ local http = require "resty.http"
 
 local httpc = http.new()
 
-local res, err = httpc:request_uri("http://user-hub/validate", {
+local res, err = httpc:request_uri("http://user-hub:5623/validate", {
     method = "POST", -- o "POST", "PUT", etc., según lo que necesites
     headers = {
         ["Content-Type"] = "application/json", -- ajusta los encabezados según tus necesidades
