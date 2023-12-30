67a7f571fb8325e6ea0cd24fffc91c1432c91014
Alexis <alexisfonzos_@hotmail.com>
2023-12-29 23:43:42 -0300

    Add query string to proxy_pass function

--- scripts/validate.lua
+++ scripts/validate.lua
@@ -48,6 +48,7 @@ local function proxy_pass(is_public)
 
     local service = match[1]
     local path = match[2]
+    local query = ngx.var.query_string
     local origin = ngx.req.get_headers()["Origin"]
 
     -- Crear una nueva instancia de HTTP client
@@ -61,10 +62,10 @@ local function proxy_pass(is_public)
         headers_table[k] = v
     end
 
-    ngx.log(ngx.STDERR, "full: ", "http://" .. service .. path)
+    ngx.log(ngx.STDERR, "full: ", "http://" .. service .. path .. query)
 
     -- Realizar la llamada a la API
-    local res, err = httpc:request_uri("http://" .. service .. path, {
+    local res, err = httpc:request_uri("http://" .. service .. path .. query, {
         method = ngx.req.get_method(), -- Utilizar el mismo método del request original
         headers = headers_table, -- Utilizar los mismos encabezados del request original
         body = ngx.req.get_body_data(), -- Utilizar el mismo cuerpo del request original
